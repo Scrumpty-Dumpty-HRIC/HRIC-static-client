c4b75a4d58550d6d7eb9280d9dda227ea05d93fe
Luke Mendiola <luke.mendi02@gmail.com>
2023-03-12 22:36:08 -0500

    outline of functions needed to utilize API

--- src/calculations.js
+++ src/calculations.js
@@ -2,90 +2,40 @@ const api = "https://hirc-api.azurewebsites.net/";
 
 console.log("Starting Health Insurance Risk Calculator... ");
 
-function calculateRisk() {
-  console.log("calculateRisk()");
+function ping() {
+  $.ajax({
+    type:"GET",
+    url: api + "/ping",
+    dataType: "text",
+    success: function (response) {
+      console.log(response);
+    }
+  })
+}
 
-  let ageResponseValue = parseInt(
-    document.getElementById("ageID").selectedOptions[0].value
-  );
-  console.log("ageResponseValue = " + ageResponseValue);
+function totalRiskCalculator() {
+  score = {
 
-  let bloodPressureResponseValue = parseInt(
-    document.getElementById("bloodID").selectedOptions[0].value
-  );
-  console.log("bloodPressureResponseValue = " + bloodPressureResponseValue);
-
-  let diabetesResponseValue = parseInt(
-    document.getElementById("diabetesID").selectedOptions[0].value
-  );
-  console.log("diabetesResponseValue = " + diabetesResponseValue);
-
-  let cancerResponseValue = parseInt(
-    document.getElementById("cancerID").selectedOptions[0].value
-  );
-  console.log("cancerResponseValue = " + cancerResponseValue);
-
-  let alzheimersResponseValue = parseInt(
-    document.getElementById("alzheimersID").selectedOptions[0].value
-  );
-  console.log("alzheimersResponseValue = " + alzheimersResponseValue);
-
-  let totalRisk =
-    ageResponseValue +
-    bloodPressureResponseValue +
-    diabetesResponseValue +
-    cancerResponseValue +
-    alzheimersResponseValue;
-  console.log("totalRisk = " + totalRisk);
-
-  document.getElementById("riskID").innerHTML =
-    "Based on the given answers, the total risk is: " + totalRisk;
-
-  let riskMessage = "Not insurable";
-  if (totalRisk <= 20) {
-    riskMessage = "This customer is Low Risk";
-  } else if (totalRisk <= 50) {
-    riskMessage = "This customer is Moderate Risk";
-  } else if (totalRisk <= 75) {
-    riskMessage = "This customer is High Risk";
   }
-
-  document.getElementById("riskMessage").innerHTML = riskMessage;
 }
 
-function BMI(height, weight) {
-  return (703 * weight) / (height * height);
+function bmiCalculator() {
+
 }
 
-function calculateBMI() {
-  console.log("CalculateBMI()");
-  let heightValueInInches = parseInt(
-    document.getElementById("heightID").selectedOptions[0].value
-  );
-  let weightValueInLbs = parseInt(
-    document.getElementById("weightID").selectedOptions[0].value
-  );
-  console.log(
-    "Height = " + heightValueInInches + " and weight = " + weightValueInLbs
-  );
+function bpCalculator() {
 
-  let bmiValue = BMI(heightValueInInches, weightValueInLbs);
-  console.log("BMI = " + bmiValue);
 
-  bmiValue = Math.round(bmiValue * 10) / 10;
-  console.log("BMI = " + bmiValue);
-
-  document.getElementById("bmiResultID").innerHTML =
-    "Your Body Mass Index is: " + bmiValue;
-
-  let bmiMessage = "Obese";
-  if (bmiValue < 24.9) {
-    bmiMessage = "Normal";
-  } else if (bmiValue < 29.9) {
-    bmiMessage = "Overweight";
-  }
-
-  document.getElementById("bmiMessage").innerHTML = bmiMessage;
 }
 
-export { calculateRisk, calculateBMI };
+function diabetesRiskCalculator() {
+
+}
+
+function cancerRiskCalculator() {
+  
+}
+
+function alzheimersRiskCalculator() {
+  
+}
